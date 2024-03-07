445fe531858805498f86691adc9546282879f5b4
philipqueen <queen.philip@gmail.com>
2024-03-06 18:58:53 -0700

    mostly working 3d data

--- ltrt/backend/realtime_pipeline.py
+++ ltrt/backend/realtime_pipeline.py
@@ -2,21 +2,27 @@ from multiprocessing.synchronize import Event as MultiprocessingEvent
 import numpy as np
 from skellytracker.trackers.base_tracker.base_tracker import BaseTracker
 from queue import Empty
+from aniposelib.cameras import CameraGroup
 
 from web_of_cams.camera_frame_buffer import CameraFrameBuffer
 
 
-def realtime_pipeline(camera_buffers: list[CameraFrameBuffer], trackers: dict[str, BaseTracker], stop_event: MultiprocessingEvent):
+def realtime_pipeline(
+    camera_buffers: list[CameraFrameBuffer],
+    trackers: dict[str, BaseTracker],
+    camera_group: CameraGroup,
+    stop_event: MultiprocessingEvent,
+):
     frame_buffers = {buffer.cam_id: None for buffer in camera_buffers}
     cutoff = int((1 / camera_buffers[0].fps) * 1e9)
 
     for buffer in camera_buffers:
         if not buffer.outbound_queue:
-            raise ValueError(
-                f"Camera {buffer.cam_id} has no outbound queue!"
-            )
+            raise ValueError(f"Camera {buffer.cam_id} has no outbound queue!")
 
+    frame_count = 0
     while not stop_event.is_set():
+        triangulate_bool = False
         for buffer in camera_buffers:
             if frame_buffers[buffer.cam_id] is None:
                 try:
@@ -33,20 +39,33 @@ def realtime_pipeline(camera_buffers: list[CameraFrameBuffer], trackers: dict[st
                 if max_timestamp - timestamp > cutoff:
                     frame_buffers[cam_id] = None  # throw out "early" frames
                     continue  # go look for next frame
-            
+
         if (
             list(frame_buffers.values()).count(None) == 0
         ):  # buffer is synchronized at this point
+            print(list(frame_buffers.values())[0][0][540, 860, :])
             tracker_data = []
             for buffer in camera_buffers:
                 trackers[buffer.cam_id].process_image(frame_buffers[buffer.cam_id][0])
-                output_array =trackers[buffer.cam_id].recorder.process_without_recording(trackers[buffer.cam_id].tracked_objects)
+                output_array = trackers[
+                    buffer.cam_id
+                ].recorder.process_without_recording(
+                    trackers[buffer.cam_id].tracked_objects
+                )
                 tracker_data.append(output_array)
-                print(output_array)
+                # print(output_array)
 
             combined_array = np.stack(tracker_data)
-            print(combined_array.shape)
+            combined_array = combined_array[:, :, :2]
+            print(combined_array[0, 0, :])
+            # print(combined_array.shape)
+            triangulate_bool = True
 
-        # TODO: pass combined array into anipose and spit out 3D data!
+        if triangulate_bool:
+            triangulated_data = camera_group.triangulate(combined_array) # TODO: try removing numba JIT
+            print(triangulated_data[0, :])
+            frame_buffers = {buffer.cam_id: None for buffer in camera_buffers} 
+            frame_count += 1
 
 
+    print(f"frame count: {frame_count}")
--- ltrt/backend/run_realtime.py
+++ ltrt/backend/run_realtime.py
@@ -1,7 +1,10 @@
 from multiprocessing import Event, Queue, Process
 from multiprocessing.synchronize import Event as MultiprocessingEvent
+from pathlib import Path
 import time
 from skellytracker import YOLOPoseTracker
+
+from aniposelib.cameras import CameraGroup
 
 from web_of_cams.__main__ import setup_cameras
 from web_of_cams.camera_frame_buffer import CameraFrameBuffer
@@ -9,7 +12,9 @@ from web_of_cams.process_handler import camera_process_handler_sm, shutdown_proc
 
 from ltrt.backend.realtime_pipeline import realtime_pipeline
 
-def run_realtime(stop_event: MultiprocessingEvent) -> tuple[list[CameraFrameBuffer], list[Process]]:
+def run_realtime(calibration_toml_path: str | Path, stop_event: MultiprocessingEvent) -> tuple[list[CameraFrameBuffer], list[Process]]:
+    camera_group = CameraGroup.load(str(calibration_toml_path))
+    
     cam_buffers = setup_cameras(fps=30)
     for buffer in cam_buffers:
         buffer.outbound_queue = Queue(maxsize=3)
@@ -22,7 +27,7 @@ def run_realtime(stop_event: MultiprocessingEvent) -> tuple[list[CameraFrameBuff
     
     realtime_pipeline_process = Process(
         target=realtime_pipeline,
-        args=(cam_buffers, trackers, stop_event)
+        args=(cam_buffers, trackers, camera_group, stop_event)
     )
     realtime_pipeline_process.start()
     processes.append(realtime_pipeline_process)
@@ -35,6 +40,14 @@ def shutdown_realtime(processes: list[Process], cam_buffers: list[CameraFrameBuf
     shutdown_processes(processes=processes, camera_buffers=cam_buffers, stop_event=stop_event)
 
 if __name__ == "__main__":
+    import cProfile
+    import pstats
     stop_event = Event()
-    cam_buffers, processes = run_realtime(stop_event)
-    shutdown_realtime(processes=processes, cam_buffers=cam_buffers, stop_event=stop_event)
+    calibration_toml_path = "/Users/philipqueen/freemocap_data/logs_info_and_settings/last_successful_calibration.toml"
+    with cProfile.Profile() as profile:
+        cam_buffers, processes = run_realtime(calibration_toml_path, stop_event)
+        shutdown_realtime(processes=processes, cam_buffers=cam_buffers, stop_event=stop_event)
+
+    # results = pstats.Stats(profile)
+    # results.sort_stats(pstats.SortKey.TIME)
+    # results.print_stats()
