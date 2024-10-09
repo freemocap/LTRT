313193b221798b8d8a82d24d31b3bcf73798d722
philipqueen <queen.philip@gmail.com>
2024-10-08 21:20:02 -0600

    add subtimers, and change how skellytracker is called

--- ltrt/backend/realtime_pipeline.py
+++ ltrt/backend/realtime_pipeline.py
@@ -30,10 +30,13 @@ def lightweight_realtime_pipeline(
 
     while not stop_event.is_set():
         # pull multiframe payload from queue
+        start_queue = perf_counter_ns()
         try:
             multiframe_payload: Optional[MultiFramePayload] = input_queue.get(timeout=0.1)
         except Empty:
             continue
+        end_queue = perf_counter_ns()
+        print(f"queue pull took {(end_queue - start_queue) / 1e6} ms")
 
         if multiframe_payload is None:
             stop_event.set()
@@ -44,20 +47,28 @@ def lightweight_realtime_pipeline(
         # TODO: Frame payloads come in an arbitrary order - should we sort by Camera ID? 
         # or, just access by order of camID (are we guaranteed to have 0-N? or can there be missing values?)
 
+        start_track = perf_counter_ns()
         tracker_data = []
         for frame_payload in multiframe_payload.frames.values():
             if frame_payload is None:
                 print(f"multiframe payload frames: {multiframe_payload.frames}")
                 raise ValueError("received None frame payload") # TODO: decide what to do for incomplete payloads
             tracker.process_image(frame_payload.image)
-            tracker_data.append(tracker.recorder.process_single_frame(tracked_objects=tracker.tracked_objects)) # this
+            tracker.recorder.record(tracked_objects=tracker.tracked_objects)
+        combined_array = tracker.recorder.process_tracked_objects()[:, :, :2]
+        tracker.recorder.clear_recorded_objects()
 
-        combined_array = np.stack(tracker_data)
-        combined_array = combined_array[:, :, :2]
+        print(f"combined array shape: {combined_array.shape}")
+
+        end_track = perf_counter_ns()
+        print(f"tracking took {(end_track - start_track) / 1e6} ms")
 
         # triangulate frame data with anipose
+        start_triangulate = perf_counter_ns()
         triangulated_data = camera_group.triangulate(combined_array)
         print(f"triangulated data shape: {triangulated_data.shape}")
+        end_triangulate = perf_counter_ns()
+        print(f"triangulation took {(end_triangulate - start_triangulate) / 1e6} ms")
         # push 3d data to output queue
         # output_queue.put(triangulated_data)
 
